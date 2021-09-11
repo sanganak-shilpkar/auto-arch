296179fe52a0bc5fb93cd19cec4d9b1e7de7ab20
Sourav Singh <88594245+sanganak-shilpkar@users.noreply.github.com>
2021-09-11 16:52:22 +0000

    added xfce4-appfinder settings

---
+++ xfce.sh
@@ -0,0 +1,15 @@
+# This file is used to customize the XFCE desktop
+
+# ----- xfce4-appfinder -----
+xfconf-query -c xfce4-appfinder -p /remember-category -s false
+xfconf-query -c xfce4-appfinder -p /always-center -s true
+xfconf-query -c xfce4-appfinder -p /enable-service -s true
+xfconf-query -c xfce4-appfinder -p /single-window -s true
+xfconf-query -c xfce4-appfinder -p /item-icon-size -s 3
+xfconf-query -c xfce4-appfinder -p /hide-category-pane -s true
+
+
+
+
+
+
