8d643d30c13d8fa289f38ba26c0a0e8622859101
Sourav Singh <88594245+sanganak-shilpkar@users.noreply.github.com>
2021-09-12 10:37:15 +0000

    refactored

--- after_chroot.sh
+++ after_chroot.sh
@@ -1,6 +1,4 @@
-# Generate locale :
-
-# uncomment needed locales
+# uncomment required locale
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 
 # generate locale
@@ -8,109 +6,114 @@ locale-gen
 
 # create locale.conf & set LANG variable
 localectl set-locale LANG=en_US.UTF-8
-
 export LANG=en_US.UTF-8
 # export LANGUAGE=$LANG
 # export LC_ALL=$LANG
 
-# Set timezone
+# set timezone
 timedatectl set-timezone Asia/Kolkata
 
-# Sync time to hardware clock
+# sync time to hardware clock
 hwclock --systohc
 
-# Add hostname entry
+# add hostname entry
 echo arch > /etc/hostname
     
-# Create the host file
+# create the host file
 touch /etc/hosts
 
-# Add following lines to the hosts file
+# add following lines to the hosts file
 echo -e 127.0.0.1'\t'localhost'\n'::1'\t\t'localhost'\n'127.0.1.1'\t'arch >> /etc/hosts
 
-# Setup root password
+# setup root password
 passwd
 
-# Generate initramfs
+# generate initramfs
 mkinitcpio -p linux
 
-# Create directory to mount EFI partition
+# create directory to mount EFI partition
 mkdir /boot/efi
 
-# Mount EFI partition
+# mount EFI partition
 mount /dev/sda1 /boot/efi
 
-# Install shell tools
+# install shell packages
 pacman -S neovim fish tmux git sudo fakeroot which --noconfirm
 
-# Install omf and theme for fish
-# curl -L https://get.oh-my.fish | fish
-
-# Install theme
-# omf install boxfish
-
-# Update fish hostname to uname -n for theme
-sed -i 's/hostname/uname -n/' ~/.config/fish/functions/fish_prompt.fish
-sed -i 's/hostname/uname -n/' ~/.config/fish/functions/fish_prompt.fish
-
-# Set defaults
+# set defaults
 echo 'export export VISUAL=nvim' | tee -a /etc/profile
 echo 'export export EDITOR=$VISUAL' | tee -a /etc/profile
 
-# Install & setup grub 
+# install & setup grub 
 pacman -S grub efibootmgr --noconfirm
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Install & setup network
+# install & setup network
 pacman -S networkmanager network-manager-applet --noconfirm
 systemctl enable NetworkManager
 
-# Install regular user
+# add regular user
 useradd -m -G wheel -s /bin/fish snowball
 
-# Set password for new user
+# set password for new user
 passwd snowball
 
-# Enable sudoers
+# enable sudo for wheel group
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
-# Install Microcode
+# install microcode
 # pacman -S amd-ucode --noconfirm
 # pacman -S intel-ucode --noconfirm
 
-# Install Graphics Driver
+# install graphics driver
 # pacman -S xf86-video-intel --noconfirm
 # pacman -S xf86-video-amdgpu --noconfirm
-#  pacman -S nvidia nvidia-utils --noconfirm
+# pacman -S nvidia nvidia-utils --noconfirm
     
-# Display Server
+# install display Server
 pacman -S xorg --noconfirm
 
-# Display Manager
+# install display manager
 pacman -S lightdm lightdm-gtk-greeter --noconfirm
 systemctl enable lightdm
 
-# Install packages for GUI
+# install desktop environment
 pacman -S xfce4 papirus-icon-theme xfce4-taskmanager xfce4-screenshooter --noconfirm
 
-# Install filemanager
-# pacman -S xarchiver unzip thunar-archive-plugin thunar-volman mtpfs libmtp gvfs gvfs-mtp android-tools android-udev --noconfirm
+# install packages for filemanager
+pacman -S xarchiver unzip thunar-archive-plugin thunar-volman mtpfs libmtp gvfs gvfs-mtp android-tools android-udev --noconfirm
 
-# Intall packages for audio
-# pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth xfce4-pulseaudio-plugin pavucontrol --noconfirm
+# intall packages for audio
+pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth xfce4-pulseaudio-plugin pavucontrol --noconfirm
 
-# Install bluetooth
-# pacman -S blueman bluez bluez-utils --noconfirm
-# systemctl enable bluetooth.service
+# install & setup bluetooth
+pacman -S blueman bluez bluez-utils --noconfirm
+systemctl enable bluetooth.service
 
-# Install Firewall
-# pacman -S gufw --noconfirm
+# install Firewall
+pacman -S gufw --noconfirm
 
-# TRIM for SSDs
-# systemctl enable fstrim.timer
+# enable TRIM for SSDs
+systemctl enable fstrim.timer
+
+# restore XFCE settings
+curl https://raw.githubusercontent.com/sanganak-shilpkar/auto-arch/main/xfce.sh > xfce.sh
+sh xfce.sh
+rm xfce
+
+# setup XFCE panel
+curl https://raw.githubusercontent.com/sanganak-shilpkar/auto-arch/main/xfce4-panel.xml > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+
+# update fish hostname to uname -n for theme
+sed -i 's/hostname/uname -n/' ~/.config/fish/functions/fish_prompt.fish
+sed -i 's/hostname/uname -n/' ~/.config/fish/functions/fish_prompt.fish
+
+# install omf and theme for fish
+curl -L https://get.oh-my.fish | fish
+omf install boxfish
 
 # Reboot
-exit
-unmount -a
-reboot
+# exit
+# unmount -a
+# reboot
